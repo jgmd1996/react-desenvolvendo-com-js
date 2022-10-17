9f16560078ebaeefee7f60c5f4e8b30eaa6f268c
Your Name <you@example.com>
2022-10-17 10:24:19 -0300

    Finalizando 1 capitulo

---
+++ Capitulo1.js
@@ -0,0 +1,5 @@
+// Criamos um novo projeto React;
+// Criamos componentes funcionais;
+// Estilizamos componentes com CSS;
+// Analisamos as dependências do projeto;
+// Analisamos os comandos disponíveis no
