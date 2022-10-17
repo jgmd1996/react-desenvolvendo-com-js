fd675cb6d0c93b2391062a4c26162602915f173a
Your Name <you@example.com>
2022-10-17 09:45:21 -0300

    Criando campo de texto

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,9 +1,13 @@
-import Banner from './componentes/Banner/Banner';
+import Banner from './componentes/Banner';
+import CampoTexto from './componentes/CampoTexto/index.js';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label="Nome"/>
+      <CampoTexto label="Cargo"/>
+      <CampoTexto label="Imagem"/>
     </div>
   );
 }
