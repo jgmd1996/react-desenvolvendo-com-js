cba28fbf08f14e31f6e217bfde549a3fde6fa11d
Your Name <you@example.com>
2022-10-18 20:12:53 -0300

    Criando card de colaborador

---
+++ organo/src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+            <img src='http://github.com/jgmd1996.png' alt=' João Gabriel'/>
+            </div>
+
+            <div className='rodape'>
+                <h4>joao gabriel</h4>
+                <h5>aluno</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
