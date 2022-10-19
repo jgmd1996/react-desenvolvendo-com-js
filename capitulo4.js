7b7405121f4d92ca8504d25f0a46bc7bd878af80
Your Name <you@example.com>
2022-10-18 21:30:58 -0300

    Escondendo times vazios e finalizando o 4 capitulo

---
+++ capitulo4.js
@@ -0,0 +1,3 @@
+// Manipular e transformar arrays de objetos;
+// Filtrar itens de um array;
+// Renderização condicional.
--- organo/src/componentes/Colaborador/index.js
+++ organo/src/componentes/Colaborador/index.js
@@ -1,5 +1,9 @@
 import './Colaborador.css'
 //http://github.com/jonathancodev.png
+//http://github.com/peas.png
+//http://github.com/Doudement-max.png
+//http://github.com/Lucas Doudement.png
+//Lucas Doudement
 const Colaborador = ({nome,imagem,cargo}) => {
     return(
         <div className='colaborador'>
