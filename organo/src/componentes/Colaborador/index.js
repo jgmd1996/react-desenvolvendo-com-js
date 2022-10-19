587ba986d1d3429aaa3c95ad7b15d2f3c7762438
Your Name <you@example.com>
2022-10-18 21:49:34 -0300

    Finalizando projeto

--- organo/src/componentes/Colaborador/index.js
+++ organo/src/componentes/Colaborador/index.js
@@ -1,13 +1,9 @@
 import './Colaborador.css'
-//http://github.com/jonathancodev.png
-//http://github.com/peas.png
-//http://github.com/Doudement-max.png
-//http://github.com/Lucas Doudement.png
-//Lucas Doudement
-const Colaborador = ({nome,imagem,cargo}) => {
+
+const Colaborador = ({nome,imagem,cargo , corDeFundo}) => {
     return(
         <div className='colaborador'>
-            <div className='cabecalho'>
+            <div className='cabecalho' style={{backgroundColor: corDeFundo}}>
             <img src={imagem} alt={nome}/>
             
             </div>
